0d084f95d7424b2ce229a51125b51c7b032322fc
aldan-ionos <alexandru-bogdan.dan@ionos.com>
2022-04-27 10:21:31 +0300

    add: Added new Benchmark test in message_test.go
    - Added new commands in the Makefile for profiling

--- Makefile
+++ Makefile
@@ -12,3 +12,20 @@ start-server:
 .PHONY: start-client
 start-client:
 	go run main.go -start-client -port=9000 -filepath=${PWD}/client/${FILENAME}
+
+.PHONY: test
+test:
+	cd ${PWD}/protos/message && go test -v .
+
+.PHONY: test-cpu-prof
+test-cpu-prof:
+	cd ${PWD}/protos/message && go test -v -cpuprofile cpu.prof .
+
+.PHONY: test-mem-prof
+test-mem-prof:
+	cd ${PWD}/protos/message && go test -v -memprofile mem.prof .
+
+.PHONY: test-bench
+test-bench:
+	cd ${PWD}/protos/message && go test -v -bench=.
+
--- protos/message/message_test.go
+++ protos/message/message_test.go
@@ -65,3 +65,65 @@ func TestMessage(t *testing.T) {
 	// Close files
 	m.CloseFiles()
 }
+
+func BenchmarkMessage(b *testing.B) {
+	var (
+		m   *message.Message
+		err error
+	)
+
+	for i := 0; i < b.N; i++ {
+		m = message.NewMessage()
+
+		// Read original file
+		err = m.OpenFile("crimeandpunishment.txt")
+		if err != nil {
+			b.Errorf("Failed to open originalFile:\n\t- %s", err.Error())
+		}
+
+		// Open New File
+		err = m.CreateFile("newfile.txt")
+		if err != nil {
+			b.Errorf("Failed to craete newFile:\n\t- %s", err.Error())
+		}
+
+		m.WaitGroup.Add(1)
+		go m.ReadOriginalFile()
+
+		m.WaitGroup.Add(1)
+		go func(m *message.Message) {
+			defer m.WaitGroup.Done()
+			for {
+				select {
+				case <-time.After(4 * time.Second):
+					// m.CloseFiles()
+					return
+				default:
+					nextLine, err := m.GetNextLine(context.Background(), &message.Void{})
+					if err != nil {
+						b.Errorf("Failed to get new line:\n\t- %s", err.Error())
+						return
+					}
+
+					if len(nextLine.NextLine) == 0 && err == nil {
+						// m.CloseFiles()
+						return
+					}
+
+					err = m.SaveNewLineToFile(nextLine.NextLine)
+					if err != nil {
+						b.Errorf("Failed to append new line to file:\n\t- %s", err.Error())
+						return
+					}
+
+				}
+
+			}
+		}(m)
+
+		m.WaitGroup.Wait()
+
+		// Close files
+		m.CloseFiles()
+	}
+}
