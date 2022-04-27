0feb12a50ef960196dce96bc58523d543e3fc743
aldan-ionos <alexandru-bogdan.dan@ionos.com>
2022-04-27 14:44:43 +0300

    update: Updated message_test based on profiling results

--- protos/message/message_test.go
+++ protos/message/message_test.go
@@ -8,11 +8,17 @@ import (
 	"github.com/aldan-ionos/go-training/protos/message"
 )
 
+var newLineList = []byte{}
+
 func TestMessage(t *testing.T) {
 	var (
 		m   = message.NewMessage()
 		err error
 	)
+
+	// defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
+	// defer profile.Start(profile.MemProfile, profile.ProfilePath(".")).Stop()
+	// defer profile.Start(profile.MutexProfile, profile.ProfilePath(".")).Stop()
 
 	// Read original file
 	err = m.OpenFile("crimeandpunishment.txt")
@@ -32,32 +38,74 @@ func TestMessage(t *testing.T) {
 	m.WaitGroup.Add(1)
 	go func(m *message.Message) {
 		defer m.WaitGroup.Done()
-		for {
-			select {
-			case <-time.After(4 * time.Second):
-				// m.CloseFiles()
-				return
-			default:
-				nextLine, err := m.GetNextLine(context.Background(), &message.Void{})
-				if err != nil {
-					t.Errorf("Failed to get new line:\n\t- %s", err.Error())
-					return
-				}
-
-				if len(nextLine.NextLine) == 0 && err == nil {
+		method := 2
+		switch method {
+		case 1:
+			for {
+				select {
+				case <-time.After(1 * time.Second):
 					// m.CloseFiles()
 					return
-				}
+				default:
+					nextLine, err := m.GetNextLine(context.Background(), &message.Void{})
+					if err != nil {
+						t.Errorf("Failed to get new line:\n\t- %s", err.Error())
+						return
+					}
 
-				err = m.SaveNewLineToFile(nextLine.NextLine)
-				if err != nil {
-					t.Errorf("Failed to append new line to file:\n\t- %s", err.Error())
-					return
+					if len(nextLine.NextLine) == 0 && err == nil {
+						// m.CloseFiles()
+						return
+					}
+
+					err = m.SaveNewLineToFile(nextLine.NextLine)
+					if err != nil {
+						t.Errorf("Failed to append new line to file:\n\t- %s", err.Error())
+						return
+					}
+
 				}
 
 			}
+		case 2:
 
+			noOfLines := 0
+			for {
+				select {
+				case <-time.After(4 * time.Second):
+					// m.CloseFiles()
+					return
+				default:
+					nextLine, err := m.GetNextLine(context.Background(), &message.Void{})
+					if err != nil {
+						t.Errorf("Failed to get new line:\n\t- %s", err.Error())
+						return
+					}
+
+					if len(nextLine.NextLine) == 0 && err == nil {
+						// m.CloseFiles()
+						return
+					}
+
+					if noOfLines == 50 {
+						err = m.SaveNewLineToFile(newLineList)
+						if err != nil {
+							t.Errorf("Failed to append new line to file:\n\t- %s", err.Error())
+							return
+						}
+						noOfLines = 0
+						newLineList = []byte{}
+
+					} else {
+						newLineList = append(newLineList, nextLine.NextLine...)
+					}
+					noOfLines++
+
+				}
+
+			}
 		}
+
 	}(m)
 
 	m.WaitGroup.Wait()
